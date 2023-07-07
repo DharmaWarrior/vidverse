0bec72ab6819acd1d746c569fc2f59304245b667
DharmaWarrior <97218268+DharmaWarrior@users.noreply.github.com>
2023-07-08 02:25:34 +0530

    first commit

---
+++ src/utils/constants.js
@@ -0,0 +1,44 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import GoogleIcon from '@mui/icons-material/Google';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://thumbs2.imgbox.com/4a/7e/26dIuigQ_t.png';
+export const writing = 'https://thumbs2.imgbox.com/05/d6/Y4HCiTJp_t.png';
+
+export const categories = [
+  { name: 'New', icon: <HomeIcon />, },
+  { name: 'Google', icon: <GoogleIcon />, },
+  { name: 'Coding', icon: <CodeIcon />, },
+  { name: 'ReactJS', icon: <CodeIcon />, },
+  { name: 'NextJS', icon: <CodeIcon />, },
+  { name: 'Music', icon: <MusicNoteIcon /> },
+  { name: 'Education', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Movie', icon: <OndemandVideoIcon />, },
+  { name: 'Gaming', icon: <SportsEsportsIcon />, },
+  { name: 'Live', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Fashion', icon: <CheckroomIcon />, },
+  { name: 'Beauty', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comedy', icon: <TheaterComedyIcon />, },
+  { name: 'Gym', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://yt3.googleusercontent.com/ytc/AGIKgqNtcsfkI815lyJKc6wUw3RkqdW0e8jiMq_OlHMeFJI=s176-c-k-c0x00ffffff-no-rj';
+export const demoChannelUrl = '/channel/UCK8sQmJBp8GCxrOtXWBpyEA';
+export const demoVideoUrl = '/video/cNfINi5CNbY';
+export const demoChannelTitle = 'Google';
+export const demoVideoTitle = 'Google Keynote (Google I/O ‘23)';
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
