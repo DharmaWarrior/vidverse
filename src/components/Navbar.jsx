775c7e7b8254c05b414ebad343e61fa10a7a51b1
Saar Agrawal <97218268+DharmaWarrior@users.noreply.github.com>
2023-12-12 00:06:11 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,9 +7,6 @@ import { SearchBar } from "./";
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2.5} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={writing} alt="writing" height={35} />
-    </Link>
     <SearchBar />
   </Stack>
 );
